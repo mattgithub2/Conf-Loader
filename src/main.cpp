373b8dcf18cedb2e677f094670f2d261dd33a493
Matt <mattwarner744@gmail.com>
2024-06-05 01:23:57 -0500

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,33 +7,39 @@
 void displayArt(WINDOW * art, int y, int x) {
 
 std::string artwork = R"(
-  _______  _                               _____  _                                                ___  
- |__   __|| |                             / ____|| |                                              / _ \ 
-    | |   | |__    ___  _ __ ___    ___  | |     | |__    __ _  _ __    __ _   ___  _ __  __   __| | | |
-    | |   | '_ \  / _ \| '_ ` _ \  / _ \ | |     | '_ \  / _` || '_ \  / _` | / _ \| '__| \ \ / /| | | |
-    | |   | | | ||  __/| | | | | ||  __/ | |____ | | | || (_| || | | || (_| ||  __/| |     \ V / | |_| |
-    |_|   |_| |_| \___||_| |_| |_| \___|  \_____||_| |_| \__,_||_| |_| \__, | \___||_|      \_/   \___/ 
-                                                                        __/ |                           
-                                                                       |___/                            
+
+  _________  _  ______  __   ____  ___   ___  _______    ________  ______
+ / ___/ __ \/ |/ / __/ / /  / __ \/ _ | / _ \/ __/ _ \  /_  __/ / / /  _/
+/ /__/ /_/ /    / _/  / /__/ /_/ / __ |/ // / _// , _/   / / / /_/ // /  
+\___/\____/_/|_/_/   /____/\____/_/ |_/____/___/_/|_|   /_/  \____/___/  
+
 )";
 
     int yPos = 0;
     std::stringstream ss(artwork);
     std::string line;
     while (std::getline(ss,line)) {
+        wattron(art, COLOR_PAIR(1));
         mvwprintw(art, y + yPos, x, "%s", line.c_str());
+        wattroff(art,COLOR_PAIR(1));
         yPos++;
     }
+
 }
 
-std::string menu_options(WINDOW * win) {
-
+void print_help() {
+	attron(COLOR_PAIR(2));
+	mvprintw(LINES - 2, 1, "Use PageUp and PageDown to scoll down or up a page of items");
+	mvprintw(LINES - 1, 1, "Arrow Keys to navigate (Ctrl c to Exit)");
+	attroff(COLOR_PAIR(2));
+}
+std::vector<std::string> get_themes(const std::string &path) {
     std::vector<std::string> themes;
-    std::ifstream in("/home/mattw/config-switcher/src/themes.txt");
+    std::ifstream in(path);
     std::string theme;
 
     if (in.fail()) {
-        std::cerr << "Could not read the themes file\n";
+        std::cerr << "Error: Could not locate themes";
         exit(EXIT_FAILURE);
     }
 
@@ -41,15 +47,20 @@ std::string menu_options(WINDOW * win) {
         themes.push_back(theme);
     }
 
+    return themes;
+}
+
+std::string menu_options(WINDOW * win, const std::vector<std::string> &vec) {
+
     int choice;
     int highlight = 0;
     for (;;) {
 
-        for (int i = 0, y_pos = 1; i < static_cast<int>(themes.size()); i++, y_pos+=2) {
+        for (int i = 0, y_pos = 2; i < static_cast<int>(vec.size()); i++, y_pos+=2) {
             if (i == highlight) {
                 wattron(win, A_REVERSE);
             }
-            mvwprintw(win, y_pos, 1, "%s", themes[i].c_str());
+            mvwprintw(win, y_pos, 1, "%s", vec[i].c_str());
             wattroff(win, A_REVERSE);
         }
         choice = wgetch(win);
@@ -63,8 +74,8 @@ std::string menu_options(WINDOW * win) {
                 break;
             case KEY_DOWN:
                 highlight++;
-                if (highlight == static_cast<int>(themes.size())) {
-                    highlight = static_cast<int>(themes.size() - 1);
+                if (highlight == static_cast<int>(vec.size())) {
+                    highlight = static_cast<int>(vec.size() - 1);
                 }
                 break; 
             default:
@@ -74,14 +85,20 @@ std::string menu_options(WINDOW * win) {
             break;
         }
     }
-    std::string selected_theme = themes[highlight];
+
+    std::string selected_theme = vec[highlight];
     return selected_theme;
 }
+
 int main(int argc, char **argv){
 
+    std::string themePath = argv[1];
+    
     initscr();
     cbreak();
-
+    start_color();
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+    init_pair(2, COLOR_CYAN, COLOR_BLACK);
     int maxY, maxX;
     
     getmaxyx(stdscr, maxY, maxX);
@@ -90,34 +107,59 @@ int main(int argc, char **argv){
 
     // Creating The windows
     WINDOW * menu = newwin(menuHeight, menuWidth, 1, 1);
-    WINDOW * preview = newwin(menuHeight, maxX - menuWidth - 1, 1, menuWidth + 1);
-    WINDOW * artwork = newwin(maxY - menuHeight - 1, maxX - 1, menuHeight + 1, 1);
+    WINDOW * options = newwin(menuHeight, maxX - menuWidth - 1, 1, menuWidth + 1);
+    WINDOW * artwork = newwin(maxY - menuHeight - 3, maxX - 1, menuHeight + 1, 1);
 
     // Refresh so the windows are recognized
     refresh();
 
     box(menu, 0, 0);
-    box(preview, 0, 0);
+    box(options, 0, 0);
     box(artwork,0,0);
 
-    mvwprintw(menu, 0,1, " Config Switcher v0 ");
-    mvwprintw(preview, 0, 1, " Preview: WIP ");
+    wattron(menu, A_BOLD);
+    mvwprintw(menu, 0,1, " Select A Theme ");
+    wattroff(menu, A_BOLD);
 
-    displayArt(artwork, (maxY - menuHeight - 1) / 3, 4); 
-    wrefresh(preview);
+    displayArt(artwork, (((maxY - menuHeight - 1) / 2) /2) - 2, 4); 
+    wrefresh(options);
     wrefresh(artwork);
 
     keypad(menu, true);
-    // wmove(menu,1,1);
+    keypad(options, true);
     curs_set(0);
     wrefresh(menu);
-    std::string theme = menu_options(menu);
 
+    print_help(); refresh();
+
+    std::vector<std::string> themes = get_themes(themePath);
+    std::string theme = menu_options(menu, themes);
+
+    wattron(options, A_BOLD);
+    mvwprintw(options, 0,1, " Select Your Window Manager ");
+    wattroff(options, A_BOLD);
+
+    std::vector<std::string> wms = {"Bspwm", "i3wm"};
+    std::string wm = menu_options(options, wms);
+
+    wclear(options); box(options,0,0);
+    wattron(options, A_BOLD);
+    mvwprintw(options, 0,1, " Select Your Terminal Emulator ");
+    wattroff(options, A_BOLD);
+
+    std::vector<std::string> terms = {"Alacritty", "Kitty"};
+    std::string term = menu_options(options, terms);
+
+
+
+    getch();
 
     endwin();
 
     std::ofstream out("tmp.txt");
-    out << theme; 
+    out << theme << "\n";
+    out << wm << "\n";
+    out << term << "\n";
     out.close();
     return 0;
 }
